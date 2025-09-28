99eb59d68fa6d31e9ad9027ea23ecf84a4ff4130
Tikhonov Vitaly <tihonovcore@gmail.com>
2025-09-28 19:21:35 +0300

    fix: add lost classes

---
+++ src/main/gen/iceberg/psi/impl/IcebergVarIdentifierImpl.java
@@ -0,0 +1,30 @@
+// This is a generated file. Not intended for manual editing.
+package iceberg.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static iceberg.psi.IcebergTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import iceberg.psi.*;
+
+public class IcebergVarIdentifierImpl extends ASTWrapperPsiElement implements IcebergVarIdentifier {
+
+  public IcebergVarIdentifierImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull IcebergVisitor visitor) {
+    visitor.visitVarIdentifier(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof IcebergVisitor) accept((IcebergVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
